780c106690ad5239b41a4028066711b7224e30c7
happydhKim <happytime870616@gmail.com>
2022-02-08 14:47:32 +0900

    [Setting] msw setting

--- src/server/handler.ts
+++ src/server/handler.ts
@@ -2,7 +2,7 @@ import { rest } from 'msw';
 
 export const handlers = () => {
   return [rest.get('/api/user', getUserInformation)];
-}
+};
 
 const getUserInformation: Parameters<typeof rest.get>[1] = (_, res, ctx) => {
   return res(
