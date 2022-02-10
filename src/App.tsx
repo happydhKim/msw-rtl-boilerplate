85df350701afea306874e0ac2efa88f22b928c03
happydhKim <happytime870616@gmail.com>
2022-02-10 20:16:51 +0900

    [Setting] 불필요한 파일 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import useSWR from 'swr';
-import logo from './logo.svg';
-import './App.css';
 
 import type { FC } from 'react';
 
@@ -18,23 +16,7 @@ const App: FC = () => {
   const { data: user } = useSWR<User>('/api/user', fetcher);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div className="App">
-        {user && `${user.name}님은 ${user.age} 살입니다.`}
-      </div>
+      {user && `${user.name}님은 ${user.age} 살입니다.`}
     </div>
   );
 };
